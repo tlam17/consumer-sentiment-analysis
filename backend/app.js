c36518ba94db44e7e76aa4a3bcb12fc86087f98c
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-02-24 16:55:03 -0700

    Added CORS

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const productsRoutes = require("./routes/productsRoutes");
 const reviewsRoutes = require("./routes/reviewsRoutes");
 const usersRoutes = require("./routes/usersRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/products", productsRoutes);
 app.use("/reviews", reviewsRoutes);
 app.use("/users", usersRoutes);
