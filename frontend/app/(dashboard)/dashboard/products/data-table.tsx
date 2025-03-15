41a8251d449412c5e2d30cea95583ce7d9051bf7
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-03-14 21:37:13 -0700

    Re-added row selection

--- frontend/app/(dashboard)/dashboard/products/data-table.tsx
+++ frontend/app/(dashboard)/dashboard/products/data-table.tsx
@@ -65,10 +65,12 @@ export function DataTable<TData, TValue>({
       onSortingChange: setSorting,
       getSortedRowModel: getSortedRowModel(),
       onColumnFiltersChange: setColumnFilters,
+      onRowSelectionChange: setRowSelection,
       getFilteredRowModel: getFilteredRowModel(),
       state: {
         sorting,
         columnFilters,
+        rowSelection,
       }
     })
    
@@ -133,18 +135,22 @@ export function DataTable<TData, TValue>({
             </Table>
           </div>
           <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">Rows per page</p>
-          <Select
-            value={`${table.getState().pagination.pageSize}`}
-            onValueChange={(value) => {
-              table.setPageSize(Number(value))
-            }}
-          >
-            <SelectTrigger className="h-8 w-[70px]">
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
-            </SelectTrigger>
+            <div className="flex-1 text-sm text-muted-foreground">
+              {table.getFilteredSelectedRowModel().rows.length} of{" "}
+              {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            <div className="flex items-center space-x-6 lg:space-x-8">
+              <div className="flex items-center space-x-2">
+                <p className="text-sm font-medium">Rows per page</p>
+                <Select
+                  value={`${table.getState().pagination.pageSize}`}
+                  onValueChange={(value) => {
+                    table.setPageSize(Number(value))
+                  }}
+                >
+                  <SelectTrigger className="h-8 w-[70px]">
+                    <SelectValue placeholder={table.getState().pagination.pageSize} />
+                  </SelectTrigger>
             <SelectContent side="top">
               {[10, 20, 30, 40, 50].map((pageSize) => (
                 <SelectItem key={pageSize} value={`${pageSize}`}>
