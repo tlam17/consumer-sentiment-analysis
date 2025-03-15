db948e236dbae7e4d6397cb9b9a77873008bcb6c
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-03-14 20:17:56 -0700

    Added pagination controls

--- frontend/app/(dashboard)/dashboard/products/columns.tsx
+++ frontend/app/(dashboard)/dashboard/products/columns.tsx
@@ -21,26 +21,6 @@ import {
 
 export const columns: ColumnDef<Product>[] = [
     {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-            checked={
-                table.getIsAllPageRowsSelected() ||
-                (table.getIsSomePageRowsSelected() && "indeterminate")
-            }
-            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-            aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-            />
-        )
-    },
-    {
         accessorKey: "product_id",
         header: "Product ID",
     },
--- frontend/app/(dashboard)/dashboard/products/data-table.tsx
+++ frontend/app/(dashboard)/dashboard/products/data-table.tsx
@@ -23,8 +23,24 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
+import { Plus } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -50,17 +66,15 @@ export function DataTable<TData, TValue>({
       getSortedRowModel: getSortedRowModel(),
       onColumnFiltersChange: setColumnFilters,
       getFilteredRowModel: getFilteredRowModel(),
-      onRowSelectionChange: setRowSelection,
       state: {
         sorting,
         columnFilters,
-        rowSelection
       }
     })
    
     return (
       <div>
-        <div className="flex items-center py-4">
+        <div className="flex items-center justify-between py-4">
             <Input
             placeholder="Filter products..."
             value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
@@ -69,6 +83,10 @@ export function DataTable<TData, TValue>({
             }
             className="max-w-sm"
             />
+            <Button>
+              <Plus/>
+              Add Product
+            </Button>
         </div>
           <div className="rounded-md border">
             <Table>
@@ -115,26 +133,70 @@ export function DataTable<TData, TValue>({
             </Table>
           </div>
           <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of{" "}
-            {table.getFilteredRowModel().rows.length} row(s) selected.
-          </div>
-            <Button
+          <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value))
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount()}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
             variant="outline"
-            size="sm"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <ChevronsLeft />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
-            >
-            Previous
-            </Button>
-            <Button
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeft />
+          </Button>
+          <Button
             variant="outline"
-            size="sm"
+            className="h-8 w-8 p-0"
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
-            >
-            Next
-            </Button>
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRight />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <ChevronsRight />
+          </Button>
+        </div>
+      </div>
         </div>
       </div>
     )
