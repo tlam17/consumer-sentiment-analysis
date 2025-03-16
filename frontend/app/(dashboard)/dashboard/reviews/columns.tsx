59585b6e994cb1a89ccaaadd165a38805477c674
tlam17 <94070835+tlam17@users.noreply.github.com>
2025-03-15 17:33:19 -0700

    Added sorting for rating, created_at, and updated_at columns

--- frontend/app/(dashboard)/dashboard/reviews/columns.tsx
+++ frontend/app/(dashboard)/dashboard/reviews/columns.tsx
@@ -3,6 +3,20 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Review } from "@/types/reviews";
 import { formatDate } from "@/utils/formatters";
+import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react"
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<Review>[] = [
     {
@@ -15,7 +29,14 @@ export const columns: ColumnDef<Review>[] = [
     },
     {
         accessorKey: "rating",
-        header: "Rating"
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Rating
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        }
     },
     {
         accessorKey: "review_text",
@@ -23,12 +44,59 @@ export const columns: ColumnDef<Review>[] = [
     },
     {
         accessorKey: "created_at",
-        header: "Created At",
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Created At
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
         cell: ({ row }) => formatDate(row.original.created_at)
     },
     {
         accessorKey: "updated_at",
-        header: "Updated At",
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Updated At
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
         cell: ({ row }) => formatDate(row.original.updated_at)
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const review = row.original;
+            
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => { 
+                            navigator.clipboard.writeText(review.review_id)
+                            toast.success("Copied to clipboard!");
+                        }}>
+                            Copy Review ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            Edit
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Delete
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
     }
 ]
